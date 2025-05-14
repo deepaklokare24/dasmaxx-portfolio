6e959ec352ca232e0255426860e3349ffb79647d
Deepak Lokare <deepaklokare.24@gmail.com>
2025-05-14 13:01:09 -0500

    fixing lint errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
@@ -14,8 +10,6 @@ import Contact from './components/Contact'
 import Footer from './components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="min-h-screen bg-white">
       <Navbar />
